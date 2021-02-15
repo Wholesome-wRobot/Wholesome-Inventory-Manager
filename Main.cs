479d1f84c543234da9d492f0fea55755cd2eb39c
Nico <jeanlouisledaron@gmail.com>
2021-02-15 17:40:33 +0100

    Fixed log spam

--- Main.cs
+++ Main.cs
@@ -16,7 +16,7 @@ public class Main : IPlugin
 
     public static Dictionary<string, bool> WantedItemType = new Dictionary<string, bool>();
 
-    public static string version = "0.0.15"; // Must match version in Version.txt
+    public static string version = "0.0.151"; // Must match version in Version.txt
 
     public void Initialize()
     {
